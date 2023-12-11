5a545b3ef9287872c13f14d591183bc645177a57
Jubair Ahmad <138237636+jubairbro@users.noreply.github.com>
2023-12-11 23:02:44 +0600

    Termux-Banner
    Termux-Banner by jubair bro

---
+++ t-ban.sh
@@ -0,0 +1,31 @@
+clear
+figlet -f big Termux Banner | lolcat
+cyan='[0;36m'
+lightgreen='[1;32m'
+red='[1;31m'
+yellow='[1;33m'
+echo -e $lightgreen "[1m                We Love Bangladesh bro... "
+echo " "
+echo -e $yellow "[1m                         -Jubair bro... "
+echo " "
+echo " "
+echo " "
+echo -e "[1m[36m
+What is Your [31mBanner [33mName[32m :
+
+"
+read varbanner
+echo
+echo -e "[1m[33m
+What is Your Benner name Name[32m :
+
+ "
+read varcowsay
+echo  "cowsay -f eyes "$varcowsay" | lolcat" > C0w54y.txt
+echo "toilet -f big ' $varbanner' -F gay | lolcat" > 84nn3r.txt
+echo
+echo "clear" > cl34r.txt
+cat "cl34r.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "C0w54y.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+ls
+cat "84nn3r.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
